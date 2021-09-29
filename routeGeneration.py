36bab3255cfe41d14110dd6cc190f1a1b950f5e0
Ryan In <ryan@wifi-172-23-43-188.uoa-wifi.auckland.ac.nz>
2021-09-30 12:29:52 +1300

    Created python files for route generation & linear program

---
+++ routeGeneration.py
@@ -0,0 +1,19 @@
+# returns a list of a list, or a dictionary of the 60 different trips for a schedule.
+
+# route generation by an algortihm
+
+# create a list of nodes/stores at a region
+
+NN = []
+N = []
+NW = []
+W = []
+CW = []
+CN = []
+CC = []
+CE = [] 
+E = []
+D = []
+DS = []
+S = []
+

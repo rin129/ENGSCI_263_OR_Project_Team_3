91090458da8a6ca6fb6510529a9b36eda8926df4
Ryan In <ryan@wifi-172-23-43-188.uoa-wifi.auckland.ac.nz>
2021-09-30 21:27:31 +1300

    Finished route generation

--- routeGeneration.py
+++ routeGeneration.py
@@ -10,44 +10,106 @@ stores = np.genfromtxt('WoolworthsTravelDurations.csv', dtype = str, delimiter =
 stores = stores[1:67]
 stores = np.delete(stores, 55, 0)
 
+weekdayDemands = np.genfromtxt('WeekdayDemands.csv', delimiter = ',', skip_header = 1, usecols = 2)
+
 travel_durations = np.genfromtxt('WoolworthsTravelDurations.csv', delimiter = ',', skip_header = 1, usecols = list(range(1,67)))
 distribution_time = np.genfromtxt('WoolworthsTravelDurations.csv', delimiter = ',', skip_header = 56, skip_footer = 10, usecols = list(range(1,67)))
 travel_durations = np.delete(travel_durations, 55, 0)
+distribution_time = np.delete(distribution_time, 55, 0)
+travel_durations = np.delete(travel_durations, 55, 1)
 
 time_threshold = 240*60
 demand_threshold = 26
 
 routes = []
+hours = []
 
 time = 0
 demand = 0
 
 for i in range(len(stores)):
-    time = 0
-    demand = 0
-    route_list = []
-    duration_store = travel_durations[i]
-    time = distribution_time[i] + (7.5*60)
-    route_list.append(stores[i])
-    # demand += demands[i]
-    while ((time < time_threshold) & (demand < demand_threshold)):
+    smallest_visited = []
+    smallest_visited.append(i)
+    for ii in range(10):
+        next_store = 0
+        time = 0
+        demand = 0
+        route_list = []
+        nodes_list = []
+
+        duration_store = travel_durations[i]
+        time = distribution_time[i] + (7.5*60)
+        route_list.append(stores[i])
+        nodes_list.append(i)
+        demand += weekdayDemands[i]
+
+        visited = False
+        thisSmallest = False
+
+        for l in range(len(duration_store)):
+            thisSmallest = l in smallest_visited
+            if (thisSmallest == True):
+                duration_store[l] = 0
+
         for j in range(len(duration_store)):
             if (duration_store[j] != 0):
                 smallest = duration_store[j]
+                next_store = j
                 break
 
         for k in range(len(duration_store)):
-            if (duration_store[k] < smallest):
+            thisSmallest = k in smallest_visited
+            visited = k in nodes_list
+            if ((duration_store[k] != 0) & (duration_store[k] < smallest) & (thisSmallest == False) & (visited == False)):
                 smallest = duration_store[k]
                 next_store = k
-        # demand += demands[i]
-        time += stores[next_store] + (7.5*60) + distribution_time[next_store]
-        if (time < time_threshold & demand < demand_threshold):
+
+        nodes_list.append(next_store)
+        smallest_visited.append(next_store)
+        demand += weekdayDemands[next_store]
+        time += duration_store[next_store] + (7.5*60) + distribution_time[next_store]
+
+        if ((time < time_threshold) & (demand <= demand_threshold)):
             time -= distribution_time[next_store]
-            distribution_time[next_store] = 0
             route_list.append(stores[next_store])
+            duration_store = travel_durations[next_store]
+            
+        else: 
+            time -= duration_store[next_store] + (7.5*60) + distribution_time[next_store] - back_home
 
-    routes.append(route_list)
+        while ((time < time_threshold) & (demand <= demand_threshold)):
 
-print(routes)
+            for j in range(len(duration_store)):
+                if (duration_store[j] != 0):
+                    smallest = duration_store[j]
+                    next_store = j
+                    break
+
+            for k in range(len(duration_store)):
+                visited = k in nodes_list
+                if ((duration_store[k] != 0) & (duration_store[k] < smallest) & (visited == False)):
+                    smallest = duration_store[k]
+                    next_store = k
+
+            demand += weekdayDemands[next_store]
+            time += duration_store[next_store] + (7.5*60) + distribution_time[next_store]
+            nodes_list.append(next_store)
+
+            if ((time < time_threshold) & (demand <= demand_threshold)):
+                back_home = distribution_time[next_store]
+                time -= distribution_time[next_store]
+                route_list.append(stores[next_store])
+                duration_store = travel_durations[next_store]
+            else: 
+                time -= duration_store[next_store] + (7.5*60) + distribution_time[next_store]
+                time += back_home
+                back_home = 0
+
+        routes.append(route_list)
+        hours.append(time/(60*60))
+
+time = 0
+print(hours)
+
+
 
